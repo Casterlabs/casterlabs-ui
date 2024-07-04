245049589f85b1fb07770da0e35765a022af1514
Ender <33337309+e3ndr@users.noreply.github.com>
2024-07-04 01:25:32 -0500

    Create Root.svelte

---
+++ src/lib/components/Root.svelte
@@ -0,0 +1,9 @@
+<div style:background-color="var(--base-1)" style:color="var(--base-12)" style:min-width="100vw" style:min-height="100vh" style:overflow="auto">
+	<slot />
+</div>
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+</style>
