24a27a58a2e2eeb7313f8437b2e5e23f38770172
StarCaim <marshall.l.bohn@gmail.com>
2024-08-06 20:00:46 -0500

    forgot this file for exports lol

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,8 @@
 export { default as CheckboxInput } from './form/CheckboxInput.svelte';
 export { default as NumberInput } from './form/NumberInput.svelte';
+export { default as RangeInput } from './form/RangeInput.svelte';
+export { default as RangeInputWithSister } from './form/RangeInputWithSister.svelte';
+export { default as TextInput } from './form/TextInput.svelte';
+export { default as Box } from './Box.svelte';
+export { default as Icon } from './Icon.svelte';
+export { default as Root } from './Root.svelte';
