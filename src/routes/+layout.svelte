b9dc35792ed776692763fe1f43958d7373ba24c1
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-05-03 01:46:20 -0500

    Fix the demo color palette.
    oops.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,18 +17,18 @@
 		--clui-color-base-11: #b5b2bc; /* text-low */
 		--clui-color-base-12: #eeeef0; /* text-high */
 
-		--clui-color-accent-1: #fffcfd; /* background-app */
-		--clui-color-accent-2: #fef7f9; /* background-subtle */
-		--clui-color-accent-3: #ffe9f0; /* element-background */
-		--clui-color-accent-4: #fedce7; /* element-hovered */
-		--clui-color-accent-5: #facedd; /* element-active  */
-		--clui-color-accent-6: #f3bed1; /* border-subtle */
-		--clui-color-accent-7: #eaacc3; /* border-element */
-		--clui-color-accent-8: #e093b2; /* border-hovered */
+		--clui-color-accent-1: #191114; /* background-app */
+		--clui-color-accent-2: #201318; /* background-subtle */
+		--clui-color-accent-3: #381525; /* element-background */
+		--clui-color-accent-4: #4d122f; /* element-hovered */
+		--clui-color-accent-5: #5c1839; /* element-active  */
+		--clui-color-accent-6: #6d2545; /* border-subtle */
+		--clui-color-accent-7: #873356; /* border-element */
+		--clui-color-accent-8: #b0436e; /* border-hovered */
 		--clui-color-accent-9: #e93d82; /* solid */
-		--clui-color-accent-10: #df3478; /* solid-hovered */
-		--clui-color-accent-11: #cb1d63; /* text-low */
-		--clui-color-accent-12: #621639; /* text-high */
+		--clui-color-accent-10: #ee518a; /* solid-hovered */
+		--clui-color-accent-11: #ff92ad; /* text-low */
+		--clui-color-accent-12: #fdd3e8; /* text-high */
 	}
 
 	:global(html) {
