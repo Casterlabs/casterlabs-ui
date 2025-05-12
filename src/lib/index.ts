0e41efdd469902ad7c5a4aa4e4b53cfa79eeb2bd
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-05-12 00:13:38 -0500

    Move these into the new helpers sub-dir.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,9 @@
 export { default as Box } from '$lib/components/Box.svelte';
 export { default as Button } from '$lib/components/Button.svelte';
 export { default as Divider } from '$lib/components/Divider.svelte';
-export { default as DynamicList } from '$lib/components/DynamicList.svelte';
 export { default as Input } from '$lib/components/Input.svelte';
-export { default as InvertedScroller } from '$lib/components/InvertedScroller.svelte';
+
+export { default as DynamicList } from '$lib/helpers/DynamicList.svelte';
+export { default as InvertedScroller } from '$lib/helpers/InvertedScroller.svelte';
 export { default as FocusTrap } from '$lib/helpers/FocusTrap.svelte';
 export { default as LongPressListener } from '$lib/helpers/LongPressListener.svelte';
